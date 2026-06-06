4e68949b16758c34b6850877381881fc7503f98a
Ricardo Piro-Rael <fdisk@fdisk.co>
2026-06-06 14:56:28 -0700

    Don't refresh home-manager every time

--- build_linux.sh
+++ build_linux.sh
@@ -2,5 +2,5 @@
 
 echo "Building Arke for Arch Linux..."
 cat arke.txt
-nix run --refresh --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- switch --flake ".#linux" --show-trace
-nix run --refresh --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- news --flake ".#linux"
+nix run --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- switch --flake ".#linux" --show-trace
+nix run --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- news --flake ".#linux"
--- build_macos.sh
+++ build_macos.sh
@@ -1,3 +1,3 @@
 cat arke.txt
-sudo nix run --refresh --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin -- switch --flake ".#macbook" --show-trace
-nix run --refresh --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- news --flake ".#macbook"
+sudo nix run --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin -- switch --flake ".#macbook" --show-trace
+nix run --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- news --flake ".#macbook"
