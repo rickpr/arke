d356e19f4c87f7c5bec684054ad624c9e2867bb6
Ricardo Piro-Rael <fdisk@fdisk.co>
2026-06-13 22:24:28 -0700

    Fix news warning

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
-echo "Building Arke for Arch Linux..."
-cat arke.txt
 nix run --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- switch --flake ".#linux" --show-trace
-nix run --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- news --flake ".#linux"
+nix run --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- news --flake ".#linux" 2>/dev/null | awk -v RS='' '/\[unread\]/'
--- build_macos.sh
+++ build_macos.sh
@@ -1,3 +1,2 @@
-cat arke.txt
 sudo nix run --extra-experimental-features nix-command --extra-experimental-features flakes nix-darwin -- switch --flake ".#macbook" --show-trace
-nix run --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- news --flake ".#macbook"
+nix run --extra-experimental-features nix-command --extra-experimental-features flakes home-manager -- news --flake ".#macbook" 2>/dev/null | awk -v RS='' '/\[unread\]/'
