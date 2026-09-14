c74a412c63ad96d12f617fb8ea45700a6e11bf6e
Ricardo Piro-Rael <ricardo@gaintain.co>
2026-09-13 18:57:34 -0700

    zsh: add Homebrew and Nix user profile to PATH via zshenv and sessionVariables

--- home.nix
+++ home.nix
@@ -128,8 +128,13 @@
         }
       ];
 
+      envExtra = ''
+        export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/etc/profiles/per-user/${user}/bin:$PATH"
+        [[ ! -f ~/.zshenv_local ]] || source ~/.zshenv_local
+      '';
+
       sessionVariables = {
-        PATH = "$HOME/.local/bin:$HOME/.config/emacs/bin:$PATH";
+        PATH = "/opt/homebrew/bin:/opt/homebrew/sbin:/etc/profiles/per-user/${user}/bin:$HOME/.local/bin:$HOME/.config/emacs/bin:$PATH";
         EDITOR = "nvim";
         VISUAL = "nvim";
       };
